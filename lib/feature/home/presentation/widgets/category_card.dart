d325e918a6a9e1a103e6cef00d7e6bebe3d98d40
moibrahemm-sourcecode <moibrahemm.sourcecode@gmail.com>
2025-08-06 14:12:09 +0300

    Added CategoryGridView

---
+++ lib/feature/home/presentation/widgets/category_card.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jomla_market/core/resources/colors/colors.dart';
+import 'package:jomla_market/feature/home/data/models/category_model.dart';
+
+class CategoryCard extends StatelessWidget {
+  final CategoryModel categoryModel;
+  final VoidCallback onTap;
+
+  const CategoryCard({
+    super.key,
+    required this.onTap,
+    required this.categoryModel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(20.r),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                width: 100.w,
+                height: 90.h,
+                decoration: BoxDecoration(
+                  color: AppColors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.r),
+                    bottomLeft: Radius.circular(20.r),
+                    bottomRight: Radius.circular(20.r),
+                  ),
+                  border: Border.all(color: Colors.grey.shade400, width: 1.w),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade300,
+                      blurRadius: 6.r,
+                      spreadRadius: 0,
+                      offset: Offset(1.w, 6.h),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                top: -15.h,
+                left: 0,
+                right: 0,
+                child: Image.asset(
+                  categoryModel.image,
+                  width: 80.w,
+                  height: 80.w,
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 8.h),
+          Text(
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            categoryModel.title,
+            style: TextStyle(fontSize: 12.sp, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/feature/home/presentation/widgets/home_view_body.dart
+++ lib/feature/home/presentation/widgets/home_view_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jomla_market/core/resources/images/images.dart';
 import 'package:jomla_market/feature/home/data/models/category_model.dart';
+import 'package:jomla_market/feature/home/presentation/widgets/category_card.dart';
 import 'package:jomla_market/feature/home/presentation/widgets/category_title.dart';
 import 'package:jomla_market/feature/home/presentation/widgets/featured_category_card.dart';
 import 'package:jomla_market/feature/home/presentation/widgets/location_selector.dart';
@@ -11,6 +12,17 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<CategoryModel> categories = [
+      CategoryModel(title: 'البسكويت والحلويات', image: AppImages.biscuits),
+      CategoryModel(title: 'البسكويت والحلويات', image: AppImages.biscuits),
+      CategoryModel(title: 'البسكويت والحلويات', image: AppImages.biscuits),
+      CategoryModel(title: 'البسكويت والحلويات', image: AppImages.biscuits),
+      CategoryModel(title: 'البسكويت والحلويات', image: AppImages.biscuits),
+      CategoryModel(title: 'البسكويت والحلويات', image: AppImages.biscuits),
+      CategoryModel(title: 'البسكويت والحلويات', image: AppImages.biscuits),
+      CategoryModel(title: 'البسكويت والحلويات', image: AppImages.biscuits),
+      CategoryModel(title: 'البسكويت والحلويات', image: AppImages.biscuits),
+    ];
     return SingleChildScrollView(
       physics: const BouncingScrollPhysics(),
       child: Padding(
@@ -24,6 +36,24 @@ class HomeViewBody extends StatelessWidget {
               categoryModel: CategoryModel(
                 title: 'البسكويت والحلويات',
                 image: AppImages.biscuits,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 22.h),
+              child: GridView.builder(
+                itemCount: 9,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 20.h,
+                  crossAxisSpacing: 20.w,
+                  childAspectRatio: 0.8.w,
+                ),
+                itemBuilder: (context, index) => CategoryCard(
+                  onTap: () {},
+                  categoryModel: categories[index],
+                ),
               ),
             ),
           ],
